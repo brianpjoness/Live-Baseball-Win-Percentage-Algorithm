90f9bf306dedf1ee71e4432cb3d7fa7f441ba80b
brianjones <brianpjoness@gmail.com>
2025-07-29 22:58:37 -0500

    Good progress. I added a logistic regression model with analysis (the model is about 73% accurate) as well as model accuracy be different game situations (inning and score difference)

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import GridSearchCV
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-df = statcast(start_dt = "2023-04-01", end_dt="2023-04-7")
+df = statcast(start_dt = "2023-04-01", end_dt="2023-04-30")
 
 
 keep_cols = [
@@ -66,10 +67,12 @@ def prepare_modeling_data(df):
 
     modeling_df = df[feature_cols + [target, "game_pk", "game_date"]].dropna()
 
-    print("Dataframe Shape")
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    print("Dataframe Shape: ")
     print(modeling_df.shape)
     print("Target Distribution: ")
     print(modeling_df.value_counts(normalize=True))
+    print("--------------------------------------------------------------------------------------------------------------------------")
     return modeling_df, feature_cols, target
 
 modeling_df, feature_cols, target = prepare_modeling_data(df)
@@ -85,40 +88,123 @@ def temporal_train_test_split(df, test_size =0.2):
     return train_df, test_df
 
 # return train and test df
-print(temporal_train_test_split(modeling_df))
+train_df, test_df = temporal_train_test_split(modeling_df)
 
 def train_baseline_models(train_df, test_df, feature_names):
     # train logistic regression and random forest models
     # make sure to do scaler for logistic regression
     # do model results and feature importances
-    hi = 1
+
+    scaler = StandardScaler()
+    X_train= scaler.fit_transform(train_df[feature_names])
+    y_train = train_df["is_batting_team_winner"]
+
+    X_test =  scaler.fit_transform(test_df[feature_names])
+    y_test = test_df["is_batting_team_winner"]
+
+    log_reg = LogisticRegression(penalty="l1", C=0.01, max_iter=100, class_weight="balanced", solver="liblinear")
+    log_reg.fit(X_train, y_train)
+    y_pred = log_reg.predict(X_test)
 
 
 
-# return models and results, print importanec
+    # regression results
+    print("Logistic Regression Results: ")
+
+    # return models and results, print importanec
+
+    print("Accuracy: ", accuracy_score(y_test, y_pred))  # percentage of correct predictions out of all predictions
+    print(classification_report(y_test, y_pred)) # precision, recall, f1 score
+    print("ROC AUC: ", roc_auc_score(y_test, log_reg.predict_proba(X_test)[:, 1])) # receiver operating characteristic area under the curve
+    # shows how well classifier seperates positive and negative classes 1 is perfect 0.5 is guessing
+    print("Log Loss: ", log_loss(y_test, log_reg.predict_proba(X_test))) # how confident the model is in its predictions lower is better (0 perfect)
+    print("--------------------------------------------------------------------------------------------------------------------------")
+
+    # split up the param grid so i can try lbfgs since it only works with l2
+    param_grid = [
+        {
+            'solver': ['lbfgs'],
+            'penalty': ['l2'],
+            'C': [0.01, 0.1, 1, 10, 100],
+            'max_iter': [100, 200],
+            'class_weight': [None, 'balanced'],
+        },
+        {
+            'solver': ['liblinear'],
+            'penalty': ['l1', 'l2'],
+            'C': [0.01, 0.1, 1, 10, 100],
+            'max_iter': [100, 200],
+            'class_weight': [None, 'balanced'],
+        },
+        {
+            'solver': ['saga'],
+            'penalty': ['l1', 'l2'],
+            'C': [0.01, 0.1, 1, 10, 100],
+            'max_iter': [100, 200],
+            'class_weight': [None, 'balanced'],
+        }
+    ]
+    model = LogisticRegression()
+    grid = GridSearchCV(model, param_grid, cv=5)
+    grid.fit(X_train, y_train)
+
+    print("Best Parameters: ", grid.best_params_)
+    print("--------------------------------------------------------------------------------------------------------------------------")
+    return X_train, X_test, y_train, y_test, y_pred, log_reg
+
+
+X_train, X_test, y_train, y_test, y_pred, log_reg = train_baseline_models(train_df, test_df, feature_cols)
 
 
 
+def analyze_predictions_by_game_situation(df, y_predictions, y_test):
+    # inning, score dif, on base
+    def create_score_diff_buckets(score_diff):
+        # converts raw score into different categories
+        if score_diff >= 5:
+            return "Up by 5+"
+        elif score_diff >= 2:
+            return "Up by 2-4"
+        elif score_diff == 1:
+            return "Up by 1"
+        elif score_diff == 0:
+            return "Tied"
+        elif score_diff == -1:
+            return "Down by 1"
+        elif score_diff >= -4:
+            return "Down by 2-4"
+        else:
+            return "Down by 5+"
 
-def evaluate_models(results):
-    # compare model performance
-    # make dataframe of the comparison with results
-    # you can also plot it if you want
 
-    # return the comparison
-    hi = 1
+    predictions_df = df[["inning", "score_diff"]].copy() # double brackets
+    predictions_df["Predicted Result"] = y_predictions
+    predictions_df["Actual Result"] = y_test
+    predictions_df["score_diff"] = predictions_df["score_diff"].apply(create_score_diff_buckets)
 
-def analyze_predictions_by_game_situation(df, predictions, target):
-    # analyze how model performs in differnet game situations
-    # with an analysis_df you can have a column for predictions and correct (.0.5)
+    inning_summary = predictions_df.groupby("inning").apply(
+        lambda group: pd.Series({       # variable group is dataframe containing all the rows for a specific inning
+            "accuracy": (group["Predicted Result"] == group["Actual Result"]).mean(), # if predicted result is the same as actual result it is assigned a 1, ow its a 0
+            # then you take the average of the 1s and 0s and this gives the accuracy
+            "count": len(group)   # this makes a column for how many rows are in each innings df
+        }),
+        include_groups=False # gets rid of depracation warning
+    )
 
-    # you can do performance of the model based on inning
+    score_diff_summary = predictions_df.groupby("score_diff").apply(
+        lambda group: pd.Series({
+            "accuracy": (group["Predicted Result"] == group["Actual Result"]).mean(),
+            "count": len(group)
+        }),
+        include_groups=False
+    )
 
-    # you can make a score diff bucket based off of how accurate the model is depending on different score difference ranges
 
-    # same thing for runners on base
-    hi = 1
 
+    return (inning_summary,
+            score_diff_summary)
+
+print(analyze_predictions_by_game_situation(test_df, y_pred, y_test))
 
 def run_modeling_pipeline(df):
     # finsih modeling pipeline
