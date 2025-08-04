a861ebeca20afe69456e86d322baf5c052aa0f81
brianjones <brianpjoness@gmail.com>
2025-08-03 22:00:31 -0500

    Tried switching to RF then tried XGB but suprisingly they are not much better than LR if at all. I am going to stick with XGB though. Also I attempted to add new features like win percentage but somehow it make the predictions worse. It may be bc of data leakage or that towards the start of the season teams may have more extreme win percentages.

--- trained_model.py
+++ trained_model.py
@@ -9,11 +9,14 @@ from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import GridSearchCV
+
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
+from xgboost import XGBClassifier
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, HalvingRandomSearchCV
 
-df = statcast(start_dt = "2023-04-01", end_dt="2023-04-30")
+df = statcast(start_dt = "2023-04-01", end_dt="2023-07-30")
 
 
 keep_cols = [
@@ -51,16 +54,21 @@ df['is_batting_team_winner'] = (df['batting_team'] == df['winner']).astype(int)
 df = df.drop(columns="winner")
 
 
+
 def prepare_modeling_data(df):
+
+
+
     feature_cols = [
-        'score_diff',           # Current score difference (batting team perspective)
+        'score_diff',           # Current score df (batting pov)
         'is_top_inning',        # Whether it's top of inning
         'Outs',                 # Number of outs
         'runners_on',           # Number of runners on base
         'batting_team_is_home', # Whether batting team is home
         'inning',               # What inning it is
         'balls',                # Current ball count
-        'strikes'               # Current strike count
+        'strikes'             # Current strike count
+
     ]
     target = 'is_batting_team_winner'
  # return modeling df, feature cols, target
@@ -106,22 +114,39 @@ def train_baseline_models(train_df, test_df, feature_names):
     # if you fit_transform on the test it is data leakage
     y_test = test_df["is_batting_team_winner"]
 
-    log_reg = LogisticRegression(penalty="l1", C=0.01, max_iter=100, class_weight="balanced", solver="liblinear")
-    log_reg.fit(X_train, y_train)
-    y_pred = log_reg.predict(X_test)
+    # logistic regression model
+    # rf = LogisticRegression(penalty="l1", C=0.01, max_iter=100, class_weight="balanced", solver="liblinear")
+    # rf.fit(X_train, y_train)
+    # y_pred = rf.predict(X_test)
 
+    # random forest model
+    # rf = RandomForestClassifier(n_estimators=150,        # number of trees in the forest
+    #                         max_depth=10,          # let trees grow fully (can overfit on small data)
+    #                         min_samples_split=5,     # minimum number of samples to split a node
+    #                         min_samples_leaf=5,      # minimum samples required at a leaf node
+    #                         max_features='log2',     # √num_features, good for classification
+    #                         bootstrap=True,          # whether to sample with replacement
+    #                         random_state=42,         # for reproducibility
+    #                         n_jobs=-1,
+    #                         class_weight="balanced")
+    # rf.fit(X_train, y_train)
+    # y_pred = rf.predict(X_test)
+
+    xgb = XGBClassifier(n_estimators=100, max_depth=None, learning_rate=0.05)
+    xgb.fit(X_train, y_train)
+    y_pred = xgb.predict(X_test)
 
 
     # regression results
-    print("Logistic Regression Results: ")
+    print("Model Results: ")
 
     # return models and results, print importanec
 
     print("Accuracy: ", accuracy_score(y_test, y_pred))  # percentage of correct predictions out of all predictions
     print(classification_report(y_test, y_pred)) # precision, recall, f1 score
-    print("ROC AUC: ", roc_auc_score(y_test, log_reg.predict_proba(X_test)[:, 1])) # receiver operating characteristic area under the curve
+    print("ROC AUC: ", roc_auc_score(y_test, xgb.predict_proba(X_test)[:, 1])) # receiver operating characteristic area under the curve
     # shows how well classifier seperates positive and negative classes 1 is perfect 0.5 is guessing
-    print("Log Loss: ", log_loss(y_test, log_reg.predict_proba(X_test))) # how confident the model is in its predictions lower is better (0 perfect)
+    print("Log Loss: ", log_loss(y_test, xgb.predict_proba(X_test))) # how confident the model is in its predictions lower is better (0 perfect)
     print("=" * 50)
 
     # split up the param grid so i can try lbfgs since it only works with l2
@@ -155,13 +180,35 @@ def train_baseline_models(train_df, test_df, feature_names):
     # print("Best Parameters: ", grid.best_params_)
     # print("-" * 50)
 
+    # random forest param grid
+
+    # param_grid = {
+    #     'n_estimators': [150, 200],  # Number of trees in the forest
+    #     'max_depth': [5, 10, 15],  # Maximum depth of trees
+    #     'min_samples_split': [6, 5, 4,],  # Min samples required to split a node
+    #     'min_samples_leaf': [5, 7, 10],  # Min samples required at each leaf node
+    #     'max_features': ["log2"],  # Number of features to consider at each split
+    #     'bootstrap': [True, False],  # Whether bootstrap samples are used
+    #     'class_weight': [None, 'balanced'],
+    #     "n_jobs": [-1]
+    # }
+    # model = RandomForestClassifier()
+    # grid = HalvingRandomSearchCV(model, param_grid, cv=5)  # apparentl gridsearchcv is just as good since we dont have too many features
+    # grid.fit(X_train, y_train)
+    # print("Best Parameters: ", grid.best_params_)
+    # print("-" * 50)
+
+
+
+
     # get the importances of the features
-    feature_importance = pd.Series(log_reg.coef_[0], index=feature_names)
+    # feature_importance = pd.Series(rf.coef_[0], index=feature_names) # this is how you do feature importance for log reg
+    feature_importance = pd.Series(xgb.feature_importances_, index=feature_names)  # this is how you do feature importance for rf
     print("Feature Importances: ")
     print(feature_importance)
     print("=" * 50)
 
-    return X_train, X_test, y_train, y_test, y_pred, log_reg, scaler
+    return X_train, X_test, y_train, y_test, y_pred, xgb, scaler
 
 
 def save_model_and_scalar(model, scaler):
@@ -228,12 +275,12 @@ def analyze_predictions_by_game_situation(df, y_predictions, y_test):
 def run_modeling_pipeline(df):
     modeling_df, feature_cols, target = prepare_modeling_data(df)
     train_df, test_df = temporal_train_test_split(modeling_df)
-    X_train, X_test, y_train, y_test, y_pred, log_reg, scaler = train_baseline_models(train_df, test_df, feature_cols)
+    X_train, X_test, y_train, y_test, y_pred, xgb, scaler = train_baseline_models(train_df, test_df, feature_cols)
     (analyze_predictions_by_game_situation(test_df, y_pred, y_test))
 
-    save_model_and_scalar(log_reg, scaler)
-    return log_reg, scaler
+    save_model_and_scalar(xgb, scaler)
+    return xgb, scaler
 
-run_modeling_pipeline(df)
+
 
 model, scaler = run_modeling_pipeline(df)
